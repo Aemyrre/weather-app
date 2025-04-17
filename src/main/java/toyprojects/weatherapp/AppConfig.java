2bf8d161019060984f57f3b176ca1f57a33546bc
Ramyr <myrreg@protonmail.com>
2025-04-17 17:11:57 +0800

    feature/update: fixed cached life cycle. cache only live up to 10mins only.

--- src/main/java/toyprojects/weatherapp/AppConfig.java
+++ src/main/java/toyprojects/weatherapp/AppConfig.java
@@ -1,14 +1,34 @@
 package toyprojects.weatherapp;
 
+import java.util.concurrent.TimeUnit;
+
 import org.springframework.boot.web.client.RestTemplateBuilder;
+import org.springframework.cache.CacheManager;
+import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.cache.caffeine.CaffeineCacheManager;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.client.RestTemplate;
 
+import com.github.benmanes.caffeine.cache.Caffeine;
+
 @Configuration
+@EnableCaching
 public class AppConfig {
+
     @Bean
     public RestTemplate restTemplate(RestTemplateBuilder builder) {
         return builder.build();
     }
+
+    @Bean
+    public CacheManager cacheManager() {
+        Caffeine<Object, Object> caffeineCacheBuilder = Caffeine.newBuilder()
+                .expireAfterWrite(10, TimeUnit.MINUTES)
+                .maximumSize(100);
+        CaffeineCacheManager cacheManager = new CaffeineCacheManager("currentWeather", "forecastWeather");
+        cacheManager.setCaffeine(caffeineCacheBuilder);
+        return cacheManager;
+    }
+
 }
