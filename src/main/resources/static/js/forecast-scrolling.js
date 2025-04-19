42e2904b8b41a4c78206a28a8f3b252bef3a254b
Ramyr <myrreg@protonmail.com>
2025-04-19 17:48:39 +0800

    feature/update: redesign of main content. new forcast location: beside current weather forecast. added new current weather data

--- src/main/resources/static/js/forecast-scrolling.js
+++ src/main/resources/static/js/forecast-scrolling.js
@@ -5,6 +5,6 @@ const container = document.querySelector(".forecast-cards");
 
 container.addEventListener("wheel", (event) => {
   console.log("Mousewheel detected");
-  event.preventDefault();
-  container.scrollLeft += event.deltaY;
+  // event.preventDefault();
+  // container.scrollLeft += event.deltaY;
 });
