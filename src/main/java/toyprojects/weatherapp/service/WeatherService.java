69cfd538112f843e62017e39ff1e7f22a3f591f2
Ramyr <myrreg@protonmail.com>
2025-04-25 13:24:53 +0800

    feauter/update: service interface additional comments for clarity

--- src/main/java/toyprojects/weatherapp/service/WeatherService.java
+++ src/main/java/toyprojects/weatherapp/service/WeatherService.java
@@ -10,8 +10,16 @@ public interface WeatherService {
 
     WeatherDataDTO getCurrentWeatherDataByCoordinates(double lat, double lon, String units, String lang);
 
+    /**
+     * Retrieves cached current weather data for a city. If cache is not
+     * available, calls external API.
+     */
     WeatherDataDTO getCachedCurrentWeatherDataByCity(String city, String units, String lang);
 
+    /**
+     * Retrieves cached current weather data for coordinates. If cache is not
+     * available, calls external API.
+     */
     WeatherDataDTO getCachedCurrentWeatherDataByCoordinates(double lat, double lon, String units, String lang);
 
     List<WeatherDataDTO> getListWeatherForecastByCity(String city, String units, String lang);
